6159692e53fc543179744e65105f74c9b86f5909
bazla <dobr1nya@mail.ru>
2022-09-23 15:08:04 +0300

    refactor: fix db.js

--- models/db.js
+++ models/db.js
@@ -3,25 +3,41 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
+let connection;
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
+function handleDisconnect() {
+  // Create a connection to the database
+  connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  });
 
-// open the MySQL connection
+  // open the MySQL connection
+  connection.connect(function (err) {
+    if (err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+    console.log('Successfully connected to the database.');
+  });
+
+  connection.on('error', function (err) {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
 // connection.connect((error) => {
 //   if (error) throw error;
 //   console.log('Successfully connected to the database.');
 // });
 
-export default pool;
+export default connection;
