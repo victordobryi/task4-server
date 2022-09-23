214a7f81319b24a9047562ba1c95dc0a7a522ed1
bazla <dobr1nya@mail.ru>
2022-09-24 01:39:24 +0300

    refactor: fix db.js

--- models/db.js
+++ models/db.js
@@ -1,20 +1,20 @@
-// import mysql from 'mysql';
-// import dotenv from 'dotenv';
+import mysql from 'mysql';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-// // // Create a connection to the database
-// const connection = mysql.createConnection({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DATABASE,
+// // Create a connection to the database
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+// // // open the MySQL connection
+// connection.connect((error) => {
+//   if (error) throw error;
+//   console.log('Successfully connected to the database.');
 // });
 
-// // // // open the MySQL connection
-// // connection.connect((error) => {
-// //   if (error) throw error;
-// //   console.log('Successfully connected to the database.');
-// // });
-
-// export default connection;
+export default connection;
