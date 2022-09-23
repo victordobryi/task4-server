a727ba11a11a5f2783d2b278a98ed848dd3750c8
bazla <dobr1nya@mail.ru>
2022-09-23 15:45:42 +0300

    refactor: fix db.js

--- models/db.js
+++ models/db.js
@@ -3,25 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
-
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
-
-// open the MySQL connection
-// connection.connect((error) => {
-//   if (error) throw error;
-//   console.log('Successfully connected to the database.');
+// const pool = mysql.createPool({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
 // });
 
-export default pool;
+// Create a connection to the database
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+// open the MySQL connection
+connection.connect((error) => {
+  if (error) throw error;
+  console.log('Successfully connected to the database.');
+});
+
+export default connection;
